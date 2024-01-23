118f5b057fba11dce036af26ae695004027950e3
Mark Sergienko <ffab00@gmail.com>
2024-01-23 15:31:24 +0000

    sadfas

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import { useEffect, useState, useCallback, useRef } from "react";
+import RecordButton from "./components/RecordButton";
+
+export default function Home() {
+  const [audioDevices, setAudioDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState("");
+  const [finalTranscription, setFinalTranscription] = useState("");
+  const [partialTranscription, setPartialTranscription] = useState("");
+
+  useEffect(() => {
+    async function listAudioDevices() {
+      if (typeof navigator === "undefined" || !navigator.mediaDevices) {
+        console.error("MediaDevices are not supported in this environment.");
+        return;
+      }
+
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const filteredDevices = devices.filter((d) => d.kind === "audioinput");
+        setAudioDevices(filteredDevices);
+        if (filteredDevices.length > 0) {
+          setSelectedDevice(filteredDevices[0].deviceId);
+        }
+      } catch (error) {
+        console.error("Error listing audio devices:", error);
+        alert("No audio input device found");
+      }
+    }
+
+    listAudioDevices();
+  }, []);
+
+  return (
+    <>
+      <style>{`
+        body {
+          margin: 1rem;
+        }
+        form {
+          display: flex;
+          flex-direction: column;
+          gap: 1rem;
+        }
+        form > div {
+          display: flex;
+          flex-direction: column;
+          gap: 0.5rem;
+        }
+        form,
+        #result {
+          box-sizing: border-box;
+          max-width: 500px;
+          margin: 0 auto;
+        }
+        #result {
+          margin-top: 1rem;
+          border-top: 1px solid #333;
+          padding-top: 1rem;
+          text-align: justify;
+        }
+      `}</style>
+      <form id="form">
+        <div>
+          {/* <label htmlFor="input_device">Audio input device</label> */}
+          <select
+            id="input_device"
+            name="input_device"
+            required
+            value={selectedDevice}
+            onChange={(e) => setSelectedDevice(e.target.value)}
+            disabled={audioDevices.length === 0}
+          >
+            {audioDevices.map((device) => (
+              <option key={device.deviceId} value={device.deviceId}>
+                {device.label || "Default"}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* <button
+          type="button"
+          onClick={isRecording ? stopRecording : startRecording}
+          style={{ color: isRecording ? "white" : "black" }}
+        >
+          {isRecording ? "Stop recording" : "Start recording"}
+        </button> */}
+        <RecordButton
+          selectedDevice={selectedDevice}
+          onTranscriptionData={(data) => {
+            console.log(data);
+            if (data.type === "final") {
+              setFinalTranscription((prev) => prev + data.transcription);
+              setPartialTranscription("");
+            } else {
+              setPartialTranscription(data.transcription);
+            }
+          }}
+          onStart={() => {
+            setFinalTranscription("");
+            setPartialTranscription("...");
+          }}
+        />
+      </form>
+
+      <div id="result">
+        <span id="finals">{finalTranscription}</span>
+        <span style={{ color: "red" }} id="partials">
+          {partialTranscription}
+        </span>
+      </div>
+    </>
+  );
+}
---
+++ src/components/RecordButton.js
@@ -0,0 +1,113 @@
+import { useEffect, useState, useCallback, useRef } from "react";
+import RecordRTC from "recordrtc";
+
+const SAMPLE_RATE = 48000;
+const GLADIA_KEY = process.env.REACT_APP_GLADIA_KEY;
+
+export default function RecordButton({
+  selectedDevice,
+  onTranscriptionData,
+  onStart,
+}) {
+  const [isRecording, setIsRecording] = useState(false);
+  const recorder = useRef(null);
+  const socket = useRef(null);
+
+  const stopRecording = useCallback(() => {
+    setIsRecording(false);
+    if (recorder.current) {
+      recorder.current.stopRecording(() => {
+        if (recorder.current) {
+          recorder.current.destroy();
+          recorder.current = null;
+        }
+      });
+    }
+    if (socket.current) {
+      socket.current.close();
+      socket.current = null;
+    }
+  }, []);
+
+  const startRecording = useCallback(async () => {
+    setIsRecording(true);
+    if (onStart) {
+      onStart();
+    }
+    try {
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: selectedDevice ? { deviceId: { exact: selectedDevice } } : true,
+      });
+
+      const newRecorder = new RecordRTC(audioStream, {
+        type: "audio",
+        mimeType: "audio/wav",
+        recorderType: RecordRTC.StereoAudioRecorder,
+        timeSlice: 1000,
+        ondataavailable: function (blob) {
+          if (socket.current && socket.current.readyState === WebSocket.OPEN) {
+            socket.current.send(blob);
+          }
+        },
+        sampleRate: SAMPLE_RATE,
+        desiredSampRate: SAMPLE_RATE,
+        numberOfAudioChannels: 1,
+      });
+
+      recorder.current = newRecorder;
+
+      const newSocket = new WebSocket(
+        "wss://api.gladia.io/audio/text/audio-transcription"
+      );
+      newSocket.onopen = () => {
+        const configuration = {
+          x_gladia_key: GLADIA_KEY,
+          frames_format: "bytes",
+          language_behaviour: "automatic single language",
+          model_type: "fast",
+          prosody: false,
+          reinject_context: false,
+          transcription_hint: "",
+          word_timestamps: false,
+          sample_rate: SAMPLE_RATE,
+        };
+        newSocket.send(JSON.stringify(configuration));
+      };
+      newSocket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (
+          data?.event === "transcript" &&
+          data.transcription &&
+          onTranscriptionData
+        ) {
+          onTranscriptionData(data);
+        }
+      };
+      newSocket.onerror = (error) => {
+        console.error("WebSocket error:", error);
+        stopRecording();
+      };
+      newSocket.onclose = (event) => {
+        console.warn(`WebSocket closed: [${event.code}] ${event.reason}`);
+        stopRecording();
+      };
+
+      socket.current = newSocket;
+
+      newRecorder.startRecording();
+    } catch (error) {
+      console.error("Error during the initialization:", error);
+      stopRecording();
+    }
+  }, [selectedDevice, onTranscriptionData, stopRecording]);
+
+  return (
+    <button
+      type="button"
+      onClick={isRecording ? stopRecording : startRecording}
+      style={{ color: isRecording ? "red" : "black", padding: 20 }}
+    >
+      {isRecording ? "Stop recording" : "Start recording"}
+    </button>
+  );
+}
