c7962445c0f934a897b5bb3038ba1bf0e46b0f11
Mark Sergienko <ffab00@gmail.com>
2024-02-12 22:52:35 +0100

    fix html entities

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,17 @@
+import he from "he";
+
 export default function Results({ transcription, language = "" }) {
   const key = language ? `transcription_${language}` : `transcription`;
   const lastElement = transcription[transcription.length - 1];
   const partialTranscription =
-    lastElement && lastElement.type === "partial" ? lastElement[key] : "";
+    lastElement && lastElement.type === "partial"
+      ? he.decode(lastElement[key])
+      : "";
 
   const finalTranscriptions = transcription
     .filter((data) => data.type === "final")
-    .map((data) => data[key]);
+    .map((data) => data[key])
+    .map((transcription) => he.decode(transcription));
 
   const lastThreeFinals = finalTranscriptions.slice(
     Math.max(finalTranscriptions.length - 3, 0)
